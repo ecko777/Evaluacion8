c9f3e55825aeacec00840ef29d8282e8c11e2956
Fabricio <ferbat.ecko@gmail.com>
2023-08-30 04:14:28 +0200

    initial

---
+++ app.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const app = express();
+const port = process.env.PORT || 3000;
+
+app.use(express.static('public'));
+
+app.get('/', (req, res) => {
+  res.sendFile(__dirname + '/public/index.html');
+});
+
+app.get('/about', (req, res) => {
+  res.sendFile(__dirname + '/public/about.html');
+});
+
+app.listen(port, () => {
+  console.log(`Servidor funcionando en http://localhost:${port}`);
+});
